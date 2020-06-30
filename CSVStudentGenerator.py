82fd15f7cbde74cdf6c5c3f5cc4a5ea7ac0e708c
UdayMalik <67522817+UdayMalik@users.noreply.github.com>
2020-06-30 12:44:08 -0500

    Update CSVStudentGenerator.py

--- CSVStudentGenerator.py
+++ CSVStudentGenerator.py
@@ -6,4 +6,3 @@ with open ('test.csv', 'w', newline='') as fp: #opens a blank csv file
     l = [('Test', 'Result_2', 'Result_3', 'Result_4'), (1, 2, 3, 4), (5, 6, 7, 8)] #data to be inserted into csv file
     m = zip(*l) #Pairs each string with an integer
     a.writerows(m) #calls writerows function to categorize data in csv file
-#tetstststststs
